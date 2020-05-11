9d1cb560011fa635d9a4d4354874237b1679e1f9
Shipboy <kuts.ol.vas@gmail.com>
2020-05-11 08:30:45 +0300

    add transform 11/05 8.30

--- Finaly project/src/main.js
+++ Finaly project/src/main.js
@@ -1,1 +1,34 @@
-alert("hello")
+document.addEventListener('DOMContentLoaded', function () {
+    class View {
+        showMenu() {
+
+        }
+    }
+    class Model {
+        constructor(view) {
+            this.view = view
+        }
+        showMainMenu(menu) {
+            menu.style.transform = 'translate(-350px,0)';
+        }
+    }
+    class Controller {
+        constructor(model) {
+            this.model = model
+        }
+        initClick() {
+            document.body.querySelector('.map').addEventListener('click', event => {
+                switch (event.target.id) {
+                    case '222': {
+                        this.model.showMainMenu(document.querySelector('.main-menu'));
+                    }
+                        break;
+                    default:
+                        break;
+                }
+            })
+        }
+    }
+    controller = new Controller(new Model(new View()));
+    controller.initClick();
+})
