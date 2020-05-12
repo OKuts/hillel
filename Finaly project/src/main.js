cafd8be66c43f462af05081ac5460492c1c4272c
Shipboy <kuts.ol.vas@gmail.com>
2020-05-12 20:18:34 +0300

    main menu

--- Finaly project/src/main.js
+++ Finaly project/src/main.js
@@ -7,72 +7,84 @@ document.addEventListener('DOMContentLoaded', function () {
             let i = id === 'location' ? 0 : 1;
             document.getElementById(id).innerText = input[i].value;
         }
-    }
-    class Model {
-        openTasks(arrTask, active) {
+        openTasks(arrTask, activeTask, activeProp) {
             let task = '';
             let imgTask = document.getElementById('service');
             arrTask.forEach((el, i) => {
-                task += `<div class = "set-task" id ="${'t' + i}">
-                            <div class = "service" >
-                                <img src = "${el.path}">
+                task += `<div class = "set-task" id="${'t' + i}">
+                            <div class = "service" id="${'a' + i}" >
+                                <img src = "${el.path}" id="${'d' + i}">
                             </div>
-                            ${el.name}
-                         </div>`;
+                                ${el.name}
+                        </div>`;
             })
             imgTask.innerHTML = task;
-            document.querySelectorAll('.service')[active].style.border = "1px solid gray";
-            this.openProp(arrTask[active].list, 0);
+            document.getElementById('task').innerText = arrTask[activeTask].name.toUpperCase();
+            document.getElementById(`${'a' + activeTask}`).style.border = "1px solid gray";
+            this.openProp(arrTask[activeTask].list, activeProp);
         }
         openProp(arrProp, active) {
-            console.log(arrProp);
             let prop = '';
             let menuProp = document.getElementById('set-task');
             arrProp.forEach((el, i) => {
                 prop += `<div class = "prop-task" id ="${'p' + i}">${el}</div>`;
-                console.log(el[i]);
             })
             menuProp.innerHTML = prop;
             document.querySelectorAll('.prop-task')[active].style.border = "1px solid gray";
         }
     }
+    class Model {
+
+    }
     class Controller {
         constructor(model, view) {
             this.model = model
             this.view = view
+            this.tasks = []
+            this.task = 0
+            this.prop = 0
             this.input = document.querySelectorAll('.input')
-            this.tasks = [];
         }
         getTasks() {
             const xhr = new XMLHttpRequest();
             const url = 'http://localhost:3333/ins';
             xhr.open('get', url);
             xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.onreadystatechange = function () {
+            xhr.onreadystatechange = () => {
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     this.tasks = JSON.parse(xhr.response);
-                    console.log(this.tasks)
-                    controller.model.openTasks(this.tasks, 0);
+                    this.view.openTasks(this.tasks, this.task, this.prop);
                 }
             }
             xhr.send();
+            this.initClick();
         }
         initClick() {
             document.querySelector('.map').addEventListener('click', event => {
-                switch (event.target.id) {
-                    case '222': {
+                switch (event.target.id[0]) {
+                    case 's': {
                         this.view.showMainMenu(document.querySelector('.main-menu'));
+                    }
+                        break;
+                    case 't':
+                    case 'a':
+                    case 'd':
+                    case 'p': {
+                        event.target.id[0] === 'p'
+                            ? this.prop = parseInt(event.target.id.slice(1))
+                            : this.task = parseInt(event.target.id.slice(1));
+                        console.log(event.target.id.slice(1))
+                        this.view.openTasks(this.tasks, this.task, this.prop);
                     }
                         break;
                 }
             })
             this.input.forEach(el => el.addEventListener('keyup', (event, i) => {
-                console.log(this.input);
                 this.view.changeInput(event.target.id, this.input)
             }));
         }
     }
     controller = new Controller(new Model(), new View());
     controller.getTasks();
-    controller.initClick();
+
 })
